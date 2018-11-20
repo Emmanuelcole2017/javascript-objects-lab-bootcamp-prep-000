fa10f9ea2e31b1f6683b86b65828645d085369b7
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-11-20 22:49:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
 }
+
+function destructivelyUpdateObjectWithKayAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
+
